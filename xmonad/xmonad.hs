bf13b952c5c79137f96bcf2106af8659143e9cb0
Arthur Caillau <arthur.caillau@airbnb.com>
2017-01-22 17:37:59 -0800

    [xmonad] clean up config files

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,13 +1,15 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
-import Graphics.X11.ExtraTypes.XF86
-import XMonad.Layout.Maximize
-import XMonad.Layout.ToggleLayouts
+import           Graphics.X11.ExtraTypes.XF86
+import           System.IO
+import           XMonad
+import           XMonad.Actions.UpdatePointer (updatePointer)
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Layout.Maximize
+import           XMonad.Layout.ToggleLayouts
+import           XMonad.Util.EZConfig         (additionalKeys)
+import           XMonad.Util.Run              (spawnPipe)
 
+-- Use urxvt as terminal
 term = "urxvt"
 
 myLayout = toggleLayouts Full tiled ||| toggleLayouts Full (Mirror tiled)
@@ -25,6 +27,8 @@ myLayout = toggleLayouts Full tiled ||| toggleLayouts Full (Mirror tiled)
      delta   = 3/100
 
 main = do
+
+    -- Starts xmobar
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ defaultConfig
@@ -35,19 +39,42 @@ main = do
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "yellow" "" . shorten 50
                         }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+                        -- Follow mouse to selected buffer
+                        >> updatePointer (0.5, 0.5) (0.5,0.5)
+
+        -- Rebind Mod to the Windows key
+        , modMask = mod4Mask
+
+        -- Rebind terminal to `term`
         , terminal = term
+
+        -- Background color for focused window
         , focusedBorderColor = "#ffff00"
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+
+        -- Screensaver
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
+
+        -- Terminal
         , ((mod4Mask, xK_Return), spawn term)
+
+        -- Layout toggle
         , ((mod4Mask .|. controlMask, xK_space), sendMessage ToggleLayout)
         , ((mod4Mask, xK_z), sendMessage (Toggle "Full"))
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+
+        -- Screenshots
+        -- , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        -- , ((mod4Mask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((mod4Mask .|. shiftMask, xK_s), spawn "scrot ~/Pictures/screen_%Y-%m-%d-%H-%M-%S.png -d 1") -- with a delay of 1 second
+        , ((mod4Mask, xK_s),               spawn "scrot ~/Pictures/screen_%Y-%m-%d-%H-%M-%S.png -d 1") -- with a delay of 1 second
+        -- , ((0, xK_Print), spawn "scrot")
+
+        -- Backlight
         , ((0                     ,xF86XK_MonBrightnessUp),  spawn "xbacklight -inc 10")
         , ((0                     ,xF86XK_MonBrightnessDown),  spawn "xbacklight -dec 10")
+
+        -- Sound
         , ((0                     ,xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 5%-")
         , ((0                     ,xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 5%+")
         , ((0                     ,xF86XK_AudioMute), spawn "amixer -D pulse sset Master toggle")
-        , ((0, xK_Print), spawn "scrot")
         ]
